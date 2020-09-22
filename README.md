96e3a3bec185b61adf49b271e0580756cca70dcf
Peter Jochem <peterpennrichjochem@gmail.com>
2020-09-22 10:53:29 -0500

    README.md changes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Chrono Machine Learning
+# Hopping Robot - Machine Learning
+We want to learn a function describing how a robot's foot interacts with granular material. Ultimatelty, the goal is to help build a monoped robot that can hop on deformable surfaces. I am working with (link) Dan Lynch, (link) and Juntao He to build such a robot. My job is to take a dataset gathered from a physics simualtion (Kronos) (link) and use machine learning to summarize the relationships between the data. We would like to know what the force vector is when the robot's foot makes contact with the ground. Further iterations could involve significantly increasing the size of the input space. Currently, the robot's foot is simply a flat plate. We could also learn a mapping that takes the foot's shape or mass properties as a variable parameter. 
+
+# Terradynamics Paper
+Describe the original Terradynamics paper
+
+# Project Chrono
+Project Chrono is an open source phyics simulator. It has the ability to simulate granular materials which is why we choose it over some of the other options in this area. Juntao has generated a dataset of a little more than 2,000,000 data points of how the plate (foot) interacts with a bed of granular material.   
+
+# Machine Learning Problem
+We want to know what the force vector is for a robot's foot making contact with the ground. If the ground were solid, we might approach this differently, but we would like the robot to be able to hop on deformable surfaces. The dynamic properties of these materials are far more complicated. One approach is to simulate the materials and try to learn the function, rather than modeling it analytically. We opted for the machine learning approach. So,we used Chrono to simulate a flat plate interacting with a bed of granular material. The input is a a pair of angles, gamma () and beta (). These describe the foot's orientation and direction of motion as it travels through the granular material. The label is a 2 vector of the stress per unit depth (into the granular material) experienced by the plate in both the x and z directions.  
--- src/processData.py
+++ src/processData.py
@@ -3,10 +3,7 @@ import time
 import random
 import matplotlib.pyplot as plt
 import warnings
-
 import keras 
-#from keras import models
-#from keras import layers
 
 warnings.filterwarnings('ignore') # Gets rid of future warnings
 with warnings.catch_warnings():
@@ -15,7 +12,7 @@ with warnings.catch_warnings():
     deprecation._PRINT_DEPRECATION_WARNINGS = False
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
-
+"""Wrapper class for a training instance. Makes it easier to wrangle data later"""
 class trainInstance:
 
     def __init__(self, inputVector, label):
@@ -23,6 +20,7 @@ class trainInstance:
         self.inputVector = inputVector # [gamma, beta]
         self.label = label # [x-stress, y-stress]
 
+"""Set of data and methods for creating a dataset which can be processed by Tensorflow or Keras"""
 class dataSet:
     
     def __init__(self, fileName):
@@ -35,8 +33,8 @@ class dataSet:
 
         for line in myFile: 
             
-            # [gamma, beta, depth, x-stress, y-stress]
-            x = line.split(",")
+            x = line.split(",") # # [gamma, beta, depth, x-stress, y-stress] - the csv file format
+            
             gamma = float(x[0]) / 90.0 # The angles are in degrees
             beta = float(x[1]) / 90.0
 
@@ -46,24 +44,24 @@ class dataSet:
 
             newTrainInstance = trainInstance([gamma, beta], [(x_stress/depth), (z_stress/depth)])
             self.allData.append(newTrainInstance)
-
-            #print(x_stress/depth)
-
+            
+            # Record the min and max depth for debugging and log it to console
             if (depth < self.minDepth):
                 self.minDepth = depth
             if (depth > self.maxDepth):
                 self.maxDepth = depth
     
-
         self.logStats()
         
         
     def logStats(self):
         print("The dataset was created succesfully")
-        print("The dataset has " + str(len(self.allData)) + " training instances") 
-        
-    """ Tensorflow reformatting
-    Split the lists of data into their input and outputs """
+        numInstances = len(self.allData)
+        print("The dataset has " + f"{numInstances:,}" + " training instances") 
+
+
+    """ Tensorflow reformatting. Split the lists of data 
+    into their input and outputs """
     def reformat(self, trainSet, testSet):
             
         train_inputVectors = []
@@ -83,7 +81,7 @@ class dataSet:
         return train_inputVectors, train_labels, test_inputVectors, test_labels
 
 
-"""Wrapper class for a tensorflow neural network"""
+"""Wrapper class for a Tensorflow neural network"""
 class NeuralNetwork:
 
     def __init__(self, dataset):
@@ -97,18 +95,20 @@ class NeuralNetwork:
         
         # Split data set into train and test
         np.random.shuffle(self.dataset.allData)
-
         self.trainSet = self.dataset.allData[ :int(0.80 * len(self.dataset.allData))]
-
         self.testSet = self.dataset.allData[int(0.80 * len(self.dataset.allData)): ]
 
+        # Reformat the data to make it easier to wrangle
         self.train_inputVectors, self.train_labels, self.test_inputVectors, self.test_labels = myDataSet.reformat(self.trainSet, self.testSet)
         
         # Setup the saving of the network
         #saver = tf.train.Saver()
         
         # Hyper parameters 
-        self.epochs = 10000
+        self.epochs = 10
+        #self.batchSize = 
+        #self.learningRate = 
+        #
         # numLayers ...
         # FIX ME
     
@@ -116,19 +116,18 @@ class NeuralNetwork:
     def defineGraph_tf(self):
         
         self.useTf = True
-        self.x = tf.placeholder(tf.float32, shape=(None, self.inputShape), name = 'x')
 
-        # Outputs are [stress X, stress Z]
+        self.x = tf.placeholder(tf.float32, shape=(None, self.inputShape), name = 'x')
         self.y = tf.placeholder(tf.float32, shape=(None, self.outputShape), name = 'y')
 
-        self.W1 = tf.Variable(tf.random_normal([self.inputShape, 10], stddev = 0.03), name = 'W1')
-        self.b1 = tf.Variable(tf.random_normal([10]), name = 'b1')
+        self.W1 = tf.Variable(tf.random_normal([self.inputShape, 28], stddev = 0.03), name = 'W1')
+        self.b1 = tf.Variable(tf.zeros([28]), name = 'b1')
 
-        self.W2 = tf.Variable(tf.random_normal([10, 6], stddev = 0.03), name = 'W2')
-        self.b2 = tf.Variable(tf.random_normal([6]), name = 'b2')
+        self.W2 = tf.Variable(tf.random_normal([28, 14], stddev = 0.03), name = 'W2')
+        self.b2 = tf.Variable(tf.zeros([14]), name = 'b2')
 
-        self.W3 = tf.Variable(tf.random_normal([6, 2], stddev = 0.03), name = 'W3')
-        self.b3 = tf.Variable(tf.random_normal([2]), name = 'b3')
+        self.W3 = tf.Variable(tf.random_normal([14, self.outputShape], stddev = 0.03), name = 'W3')
+        self.b3 = tf.Variable(tf.zeros([self.outputShape]), name = 'b3')
 
         #self.W4 = tf.Variable(tf.random_normal([12, 5], stddev = 0.03), name = 'W4')
         #self.b4 = tf.Variable(tf.random_normal([5]), name = 'b4')
@@ -147,10 +146,8 @@ class NeuralNetwork:
         #self.loss = tf.reduce_mean(tf.square(self.y_pred - self.y))
         
         # Derivatives of the graph
-        self.d_loss_dx = tf.gradients(self.loss, self.x)[0]
-
-        # 0.00000001
-        self.optimizer = tf.train.GradientDescentOptimizer(0.0000001)
+        self.d_loss_dx = tf.gradients(self.loss, self.x)[0]  
+        self.optimizer = tf.train.GradientDescentOptimizer(0.00000001)
         self.train_op = self.optimizer.minimize(self.loss)
 
 
@@ -169,14 +166,12 @@ class NeuralNetwork:
 
         # Set more of the model's parameters
         self.optimizer = keras.optimizers.Adam(learning_rate = 0.01)
-
-        self.network.compile(loss='mse',
-                optimizer = self.optimizer,
-                metrics=['mse']) 
         
+        # FIX ME - try removing the metrics - redundant
+        self.network.compile(loss='mse', optimizer = self.optimizer, metrics=['mse']) 
 
 
-    """ hi """
+    """Train the neural network using Tensorflow directly"""
     def train_tf(self):
         
         with tf.Session() as sess:
@@ -198,128 +193,39 @@ class NeuralNetwork:
                 loss_test_array[i] = self.loss.eval(feed_dict_test)
 
             # Feed in a real pair of data we trained on
-            # grad_numerical = sess.run(self.d_loss_dx, {self.x: [self.test_inputVectors[0] ] , self.y: [self.test_labels[0] ] } )  
-            prediction = sess.run(self.y_pred, {self.x: [self.test_inputVectors[0]], self.y: [self.test_labels[0]] } )
-            print("")
-            print("The prediction is ")
-            print(prediction)
-
+            # grad_numerical = sess.run(self.d_loss_dx, {self.x: [self.test_inputVectors[0] ], self.y: [self.test_labels[0] ] } )  
+            prediction = sess.run(self.y_pred, {self.x: [self.test_inputVectors[0]], self.y: [self.test_labels[0]]})
+            
+            # FIX ME - create a method to do this
             plt.plot(np.linspace(0, self.epochs, self.epochs), loss_test_array, color = "blue")
             plt.title('Loss Function vs Epoch - Hopping Foot')
             plt.ylabel('Loss')
             plt.xlabel('Epoch')
             plt.show()
 
-        #"""Describe"""
-        #def viewLearnedFunction(self):
-        
             angle_resolution = 20
-            # Remember we are using DEGREES
-            angle_increment = 180.0 / angle_resolution
+            angle_increment = 180.0 / angle_resolution # Remember we are using DEGREES
             
-            # Vary this
-            # depth_increment =  
-            # For experimentation - set this to the average value
-            #depth = 0.5 * (self.dataset.minDepth + self.dataset.maxDepth)
-        
-            num_depth_intervals = 20
-            depth_increment = self.dataset.maxDepth - self.dataset.minDepth
-            
-            # inputData = []
-    
-            F_X = np.zeros((angle_resolution + 1, angle_resolution + 1)) 
-            F_Z = np.zeros((angle_resolution + 1, angle_resolution + 1))
+            F_X = np.zeros((angle_resolution, angle_resolution)) 
+            F_Z = np.zeros((angle_resolution, angle_resolution))
 
-            #count = 0
             for i in range(angle_resolution):
                 for j in range(angle_resolution):
                     
                     gamma = -1 * (180.0 / 2.0) + (j * angle_increment)
                     beta = (180.0 / 2.0) - (i * angle_increment)
 
-                    nextEntry = [gamma, beta]
-            
-                    # The y-label is irrelevant to predicting the y-label, here, after training
-                    prediction = self.network.predict([nextEntry]) 
-                    #sess.run(self.y_pred, {self.x: [nextEntry] , self.y: [[ 1.0, 1.0 ]] } )
+                    # The label (self.y) is irrelevant to predicting the y here, after training
+                    # FIX ME - try removing the y
+                    prediction = sess.run(self.y_pred, {self.x: [[gamma, beta]], self.y: [[1.0, 1.0]]})
                         
-                        
-                    #if (newX < 0):
-                    #    newX = 0
-                    #if (newZ < 0):
-                    #    newZ = 0
-
                     F_X[i][j] = prediction[0][0]
-                    #F_Z[i][j] = newZ
+                    F_Z[i][j] = prediction[0][1]
                     
-
-            w = 10
-            h = 10
-            fig = plt.figure(figsize = (8, 8))
-            columns = 2
-            rows = 1
-            ax1 = fig.add_subplot(rows, columns, 1)
-            ax1.set_yticklabels([])
-            ax1.set_xticklabels([])
-            plt.imshow(F_Z)
-            ax1.set_ylabel('β')
-            ax1.set_xlabel('γ')
-            ax1.title.set_text('α_Z')
-            plt.colorbar()
-
-            ax2 = fig.add_subplot(rows, columns, 3)
-            ax2.set_yticklabels([])
-            ax2.set_xticklabels([])
-            plt.imshow(F_X)
-            ax2.set_ylabel('β')
-            ax2.set_xlabel('γ')
-            ax2.title.set_text('α_X')
-    
-            plt.colorbar()
-            plt.show()
-
+            self.createPlots(F_Z, F_X) 
         
-    def train_keras(self):
+    def createPlots(self, F_Z, F_X):
         
-        # self.train_inputVectors, self.y: self.train_labels
-        print("\n\n\n\n\nThe length of __ is " + str(len(self.train_inputVectors)) )
-        #batch_size = 1000
-        self.network.fit([self.train_inputVectors], [self.train_labels], batch_size = len(self.train_inputVectors), epochs = 4000)
-            
-        angle_resolution = 40
-        # Remember we are using DEGREES
-        angle_increment = 180.0 / angle_resolution
-
-        # Vary this
-        # depth_increment =  
-        # For experimentation - set this to the average value
-        #depth = 0.5 * (self.dataset.minDepth + self.dataset.maxDepth)
-
-        #num_depth_intervals = 20
-        #depth_increment = self.dataset.maxDepth - self.dataset.minDepth
-
-        F_X = np.zeros((angle_resolution, angle_resolution))
-        F_Z = np.zeros((angle_resolution, angle_resolution))
-            
-        for i in range(angle_resolution):
-            for j in range(angle_resolution):
-                
-                gamma = (-1*(180.0 / 2.0) + (j * angle_increment)) / 90.0 
-                beta = ((180.0 / 2.0) - (i * angle_increment)) / 90.0
-
-                nextEntry = [gamma, beta]
-
-                prediction = self.network.predict([[nextEntry]])
-
-                #if (newX < 0):
-                #newX = 0
-                #if (newZ < 0):
-                #newZ = 0
-
-                F_X[i][j] = prediction[0][0] # 0.15
-                F_Z[i][j] = prediction[0][1]
-
-
         fig = plt.figure(figsize = (4, 4))
         columns = 2
         rows = 1
@@ -331,8 +237,7 @@ class NeuralNetwork:
         ax1.set_xlabel('γ')
         ax1.title.set_text('α_Z')
         plt.colorbar()
-        
-        
+
         ax2 = fig.add_subplot(rows, columns, 2)
         ax2.set_yticklabels([])
         ax2.set_xticklabels([])
@@ -340,10 +245,33 @@ class NeuralNetwork:
         ax2.set_ylabel('β')
         ax2.set_xlabel('γ')
         ax2.title.set_text('α_X')
-        print("The (0, 0) prediction is " + str(self.network.predict([[[0, 0]]])))
         plt.colorbar()
         plt.show()
+
+        
+    def train_keras(self):
+        
+        #batch_size = 1000
+        self.network.fit([self.train_inputVectors], [self.train_labels], batch_size = len(self.train_inputVectors), epochs = 200)
             
+        angle_resolution = 40
+        angle_increment = 180.0 / angle_resolution # Remember we are using DEGREES
+
+        F_X = np.zeros((angle_resolution, angle_resolution))
+        F_Z = np.zeros((angle_resolution, angle_resolution))
+            
+        for i in range(angle_resolution):
+            for j in range(angle_resolution):
+                
+                gamma = (-1 * (180.0 / 2.0) + (j * angle_increment)) / 90.0 
+                beta = ((180.0 / 2.0) - (i * angle_increment)) / 90.0
+
+                prediction = self.network.predict([[[gamma, beta]]])
+
+                F_X[i][j] = prediction[0][0] 
+                F_Z[i][j] = prediction[0][1]
+        
+        self.createPlots(F_Z, F_X)
 
 
 # Create a datset object with all our data
@@ -351,71 +279,8 @@ myDataSet = dataSet("../dataSets/allData/compiledSet.csv")
 
 myNetwork = NeuralNetwork(myDataSet)
 
-#myNetwork.defineGraph_tf()
-#myNetwork.train_tf()
-myNetwork.defineGraph_keras()
-myNetwork.train_keras()
+myNetwork.defineGraph_tf()
+myNetwork.train_tf()
+#myNetwork.defineGraph_keras()
+#myNetwork.train_keras()
 
-
-
-"""
-    # Run the predictions with diffrent gamma, and betas
-    # Warmup: Predict the original data/test data
-
-    resolution = 20
-    # Remember we are using DEGREES
-    increment = 180.0 / resolution
-    
-    inputData = []
-    
-    F_X = np.zeros((resolution, resolution)) 
-    F_Z = np.zeros((resolution, resolution))
-
-    count = 0
-    for i in range(resolution):
-        for j in range(resolution):
-            
-            gamma = -1 * (180.0 / 2.0) + (j * increment) 
-            beta = (180.0 / 2.0) - (i * increment)
-            
-            # depth = 1.0
-            nextEntry = [gamma, beta]
-            
-            inputData.extend( [nextEntry] )
-            count = count + 1
-                
-            # The y-label is irrelevant to predicting the y-label, here, after training
-            prediction = sess.run(y_pred, {x: [nextEntry] , y: [[ 1.0, 1.0 ]] } )
-            
-            # F_X[i][j] = ( (prediction[0][0] + 0.1) / 0.2) * 255
-            # F_Z[i][j] = ( (prediction[0][1] + 0.3) / 0.6) * 255
-
-            F_X[i][j] = prediction[0][0] 
-            F_Z[i][j] = prediction[0][1]
-
-
-    w = 10
-    h = 10
-    fig = plt.figure(figsize = (8, 8))
-    columns = 3
-    rows = 1
-    ax1 = fig.add_subplot(rows, columns, 1)
-    ax1.set_yticklabels([])
-    ax1.set_xticklabels([])
-    plt.imshow(F_Z)
-    ax1.set_ylabel('β')
-    ax1.set_xlabel('γ')
-    ax1.title.set_text('α_Z')
-    plt.colorbar()
-
-    ax2 = fig.add_subplot(rows, columns, 3)
-    ax2.set_yticklabels([])
-    ax2.set_xticklabels([])
-    plt.imshow(F_X)
-    ax2.set_ylabel('β')
-    ax2.set_xlabel('γ')
-    ax2.title.set_text('α_X')
-    
-    plt.colorbar()
-    plt.show()
-"""
